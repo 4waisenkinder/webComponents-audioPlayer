a0362c3e65ca3c4d45bf755f37f405e14260008e
André Kussmann <zoddy@zoddy.de>
2013-10-13 10:19:48 +0200

    add js and grunt uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,19 @@ module.exports = function(grunt) {
         }
       }
     },
+    uglify: {
+      minify: {
+        options: {
+          report: 'min'
+        },
+        files: {
+          'audioPlayer.min.js': ['audioPlayer.js']
+        }
+      }
+    },
     watch: {
       source: {
-        files: ['audioPlayer.html', 'audioPlayer.scss'],
+        files: ['audioPlayer.html', 'audioPlayer.scss', 'audioPlayer.js'],
         tasks: ['default'],
         options: {
           atBegin: true,
@@ -36,20 +46,34 @@ module.exports = function(grunt) {
   });
 
 
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
 
   grunt.registerTask('htmlPrepare', function() {
     grunt.file.write('audioPlayer.temp.html', grunt.template.process(
       grunt.file.read('audioPlayer.html'),
-      {data: {css: grunt.file.read('audioPlayer.css')}}
+      {data: {
+        css: grunt.file.read('audioPlayer.css'),
+        js: grunt.file.read('audioPlayer.min.js')
+      }}
     ));
   });
+
   grunt.registerTask('clean', function() {
     grunt.file.delete('audioPlayer.css');
     grunt.file.delete('.sass-cache');
+    grunt.file.delete('audioPlayer.min.js');
     grunt.file.delete('audioPlayer.temp.html');
   });
-  grunt.registerTask('default', ['compass', 'htmlPrepare', 'htmlmin', 'clean']);
+
+  grunt.registerTask('default', [
+    'compass',
+    'uglify',
+    'htmlPrepare',
+    'htmlmin',
+    'clean'
+  ]);
 };
